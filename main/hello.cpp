58f7fc3360b04e7f5a5a657226c0321f24abda6f
suren <v.suren.dran280@gmail.com>
2024-03-10 12:37:55 +0530

    webhook com

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout <<"Webhook successfully enabled";
+  cout <<"Webhook successfully enabled ";
   return 0;
 }
